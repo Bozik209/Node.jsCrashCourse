9ef1b3fa9f25ee9e622ead95de0050bf7157e969
Boaz Yakobi <44434303+boaz209@users.noreply.github.com>
2022-05-19 10:14:40 +0300

    Add files via upload

---
+++ reference/path_demo.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+// Base file name
+console.log(path.basename(__filename));     // path_demo.js
+
+// Directory name
+console.log(path.dirname(__filename));      // C:\Users\Boaz\Desktop\Code\Node.js Crash Course\reference
+
+// File extension
+console.log(path.extname(__filename));      // .js
+
+// Create path object
+console.log(path.parse(__filename).base);       // path_demo.js
+
+// Concatenate paths
+console.log(path.join(__dirname, 'test', 'hello.html'));    // C:\Users\Boaz\Desktop\Code\Node.js Crash Course\reference\test\hello.html
+
+
+
+// More examples
+
+
+// get the path delimiter base on the current OS Environment
+const platSpec = path.delimiter;
+
+console.log(platSpec);
+
+
+// ge the path format on POSIX : / and Windows : \
+// more info : https://nodejs.org/dist/latest-v11.x/docs/api/path.html#path_path_format_pathobject
+const pathformat = path.format({
+  dir: pathjoin,
+  root: pathjoin,
+  base: pathjoin,
+  name: pathjoin,
+  ext: pathjoin,
+});
+
+console.log(pathformat);
+
+
+// get the parent folder director
+const parentDir = path.dirname(__dirname);
+
+console.log(parentDir);
